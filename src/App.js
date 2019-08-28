4476d0e25e4d14f0793aa0cd55d8410a225a03de
albseb511 <albertsebastian511@gmail.com>
2019-08-28 09:19:29 +0530

    components-func,props,parent->child,child->parent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/profile/Profile'
+
+
+const follow = (name='default',count=0)=> {
+  console.log('Followed:',name,'Total followers:',count)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Profile src='https://avatars1.githubusercontent.com/u/9168716?s=400&v=4' 
+                    name='Mohammed Tanveer' 
+                    user='tanveer86' 
+                    location=' Bangalore, India' 
+                    count={100}
+                    follow={(a,b)=>follow(a,b)}  />
+          <Profile src='https://avatars2.githubusercontent.com/u/53760280?s=460&v=4' 
+                  name='Suraj Singh' 
+                  user='Suraj10074' 
+                  location='' 
+                  count={50}
+                  follow={(a,b)=>follow(a,b)}  />
+      </div>
   );
 }
 
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Profile.css'
+
+
+export default class Profile extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            src:this.props.src,
+            name:this.props.name,
+            user:this.props.user,
+            location:this.props.location,
+            count:this.props.count
+        }
+    }
+
+    handleOnClick = ()=> {
+        
+        this.setState({
+            count: this.state.count+1
+        })
+
+        this.props.follow(this.state.name,this.state.count)
+    }
+
+    render(){
+        return(
+            <div>
+                <img className='img' src={this.state.src} />
+                <div className='Name'>{this.state.name}</div>
+                <div className='userId'>{this.state.user}</div>
+                <button className='btn' onClick={()=>this.handleOnClick()}>Follow : Total={this.state.count}</button>
+                <div className='location'>{this.state.location}</div>
+            </div>
+        )
+    }
+}
+
